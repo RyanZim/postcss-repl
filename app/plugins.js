54a843250268e1d357e6a4669f97c91cdbf40ec9
RyanZim <opensrc@ryanzim.com>
2017-11-24 17:06:41 -0500

    Add postcss-cssnext
    Aliasing postcss-font-family-system-ui to
    postcss-font-family-system-ui-commonjs to get this dependency working.

--- app/plugins.js
+++ app/plugins.js
@@ -4,6 +4,7 @@ module.exports = {
   'postcss-nested': require('postcss-nested'),
   'postcss-short': require('postcss-short'),
   lost: require('lost'),
+  'postcss-cssnext': require('postcss-cssnext'),
   'postcss-ordered-values': require('postcss-ordered-values'),
   'postcss-calc': require('postcss-calc'),
   'postcss-flexbugs-fixes': require('postcss-flexbugs-fixes'),
