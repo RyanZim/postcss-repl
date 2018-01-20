3b1c32ca38308ea7154805eb3b481c710f9772b7
Ryan Zimmerman <opensrc@ryanzim.com>
2018-01-20 14:17:53 -0500

    Basic Ace editor integration

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -1,9 +1,13 @@
 // This is included from App.svelte
+
+import Ace from '../Ace'
+
 const css = `p {
   color: red;
 }`;
 
 export default {
+  components: { Ace },
   data() {
     return {
       input: css,
@@ -14,37 +18,4 @@ export default {
       checkedPlugins: {},
     };
   },
-  methods: {
-    tab(event) {
-      if (event.key !== 'Tab') return;
-
-      event.preventDefault();
-      const position = event.target.selectionEnd;
-      const input = this.get('input');
-
-      if (event.shiftKey) {
-        // Ensure that we're only deleting whitespace
-        if (input.substring(position - 2, position) === '  ') {
-          this.set({
-            input: input.substring(0, position - 2) + input.substring(position),
-          });
-          setCursorPosition(event.target, position - 2);
-        }
-      } else {
-        this.set({
-          input:
-            input.substring(0, position) + '  ' + input.substring(position),
-        });
-        setCursorPosition(event.target, position + 2);
-      }
-    },
-  },
 };
-
-function setCursorPosition(element, start, end) {
-  if (typeof end === 'undefined') end = start;
-  setTimeout(() => {
-    element.focus();
-    element.setSelectionRange(start, end);
-  }, 0);
-}
