97ad9af75239abb02664bf5602288fc14722f41f
Ryan Zimmerman <opensrc@ryanzim.com>
2018-03-29 09:18:54 -0400

    Prevent duplicated normalize code

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('postcss-normalize')({ forceImport: true }),
+    require('postcss-normalize'),
     require('postcss-simple-vars'),
     require('postcss-preset-env')({ stage: 1 }),
     require('cssnano'),
